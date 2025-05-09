3437165ca542aeefb340f7a51dfeefcf216fbce8
William Erik Baxter <web@superscript.com>
2025-05-09 17:25:46 -0500

    Extend patches for freebsd

--- mail/meli/files/patch-melib_src_utils_lock.rs
+++ mail/meli/files/patch-melib_src_utils_lock.rs
@@ -8,3 +8,12 @@
              target_os = "illumos",
              target_os = "ios",
              target_os = "linux",
+@@ -81,6 +80,8 @@ where
+                        * at the location specified by l_whence and l_start through to the end of
+                        * file, no matter how large the file grows. */
+             l_pid: 0, /* "By contrast with traditional record locks, the l_pid field of that structure must be set to zero when using the commands described below." */
++            #[cfg(target_os = "freebsd")]
++            l_sysid: 0,
+         };
+         let ret_val = unsafe { libc::fcntl(fd, F_SETLK, &mut flock) };
+         log::debug!("dropped unix fd lock for mbox fd {}, got {}", fd, ret_val);
